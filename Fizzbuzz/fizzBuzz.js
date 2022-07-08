6c1a2a654bb4415b41d95b8f5c62269d090cbbee
adanbzrra <adanbzrraa@gmail.com>
2022-07-08 13:23:04 -0300

    maximo-entre-dois-valores.js e fizzBuzz.js

---
+++ Fizzbuzz/fizzBuzz.js
@@ -0,0 +1,24 @@
+// comparar alguns valores e retornar baseado em um valor de entrada.
+// divisível por 3 => Fizz
+// divisível por 5 => Buzz
+// divisível por 3 e 5 => FizzBuzz
+// não é divisivel por 3 ou 5 => entrada 
+// não é número retorna não é um número
+
+const resultado = fizzBuzz(15); //passar o valor aqui()
+
+console.log(resultado);
+
+function fizzBuzz(entrada){
+    if (typeof entrada !== 'number')
+        return 'Não é um número';
+    if (entrada % 3 === 0 && entrada % 5 === 0)
+    return 'FizzBuzz';
+    if (entrada % 3 == 0)
+        return 'Fizz';
+    if (entrada % 5 === 0)
+        return 'Buzz'    
+    if (entrada % 5 === 0)
+        return
+    return entrada;
+}
---
+++ maximo-entre-dois-valores/maximo-entre-dois-valores.js
@@ -0,0 +1,45 @@
+/*Escreva uma função que usa dois número
+ e retorna o maior enbtre eles*/
+
+ let a = 10;
+ let b = 10;
+
+ function RetornaMaior(){
+    if(a>b) {
+        return(
+            console.log('O valor de a é maior que b: ', 'a = ', a, ' b = ', b)
+        )
+    } else if(a<b) {
+        return(
+            console.log('O valor de a é menor que b:', 'a = ', a, ' b = ', b)
+        )
+    } else if(a == b) {
+        return(
+            console.log('Os valor de a é igual a b: ', 'a = ', a, ' b = ', b)
+        )
+    }
+ };
+
+ RetornaMaior();
+
+ /* Outra forma de criar */
+
+ let valorMaior2 = RetornaMaior (20, 70);
+ console.log(valorMaior2);
+
+function RetornaMaior(x, y){
+    if (x > y) {
+        return x ;
+    } else {
+        return y;
+    }
+}
+
+/* simplificando com operador ternário */
+
+let valorMaior3 = RetornaMaior (100, 700);
+ console.log(valorMaior3);
+
+function RetornaMaior(numero1, numero2){
+   return numero1 > numero2 ? numero1 : numero2 ;
+}
